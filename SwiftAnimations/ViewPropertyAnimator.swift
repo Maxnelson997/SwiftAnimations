12c952d4c855cde4bb1d1bbde5ba6b6543998048
Max <maxnelson997@gmail.com>
2019-05-19 19:55:35 -0600

    (two videos in one commit, shaking animation and the previous video) reate a UIView shaking animation with CAKeyFrameAnimation

--- SwiftAnimations/ViewPropertyAnimator.swift
+++ SwiftAnimations/ViewPropertyAnimator.swift
@@ -35,9 +35,56 @@ class ViewPropertyAnimator: UIViewController {
             self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
         }
     
-        setupCAGradient()
-        setupCABasic()
-        setupSlider()
+//        setupCAGradient()
+//        setupCABasic()
+//        setupSlider()
+//        setupCAKeyframe()
+        blurView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        perform(#selector(shakeAnimation), with: nil, afterDelay: 4)
+    }
+    
+ 
+    @objc fileprivate func shakeAnimation() {
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.values = [
+            CGPoint(x: 0, y: 0),
+            CGPoint(x: -20, y: -20),
+            CGPoint(x: 20, y: 20),
+            CGPoint(x: -10, y: -10),
+            CGPoint(x: 10, y: 10),
+            CGPoint(x: -10, y: -10),
+            CGPoint(x: 0, y: 0)
+        ]
+        animation.keyTimes = [
+            0, 0.1, 0.25, 0.4, 0.6, 0.8, 1
+        ]
+        animation.duration = 0.5
+        animation.isAdditive = true
+        animation.repeatCount = 20
+        
+        blurView.layer.add(animation, forKey: "shakeanimation")
+    }
+    
+    fileprivate func setupCAKeyframe() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "transform.scale"
+        animation.values = [0.5, 1, 0.5, 0.75]
+        animation.keyTimes = [0, 0.25, 0.5, 0.75, 1]
+        animation.duration = 2
+        // keyTime value * duration = keyTime value in seconds
+        blurView.layer.add(animation, forKey: "transformscale")
+        
+        
+        let colorAnimation = CAKeyframeAnimation(keyPath: "backgroundColor")
+        colorAnimation.values = [
+            UIColor.cyan.cgColor,
+            UIColor.red.cgColor,
+            UIColor.green.cgColor
+        ]
+        colorAnimation.keyTimes = [0, 0.5, 1]
+        colorAnimation.duration = 1
+        
+        blurView.layer.add(colorAnimation, forKey: "bgcolor")
     }
     
     fileprivate func setupCAGradient() {
